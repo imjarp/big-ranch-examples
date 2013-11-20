f14d4c3edeeabcacdbc4ff98fdfb7ecf836ee2eb
JARP <ramirezp.jorge@gmail.com>
2013-11-19 23:59:17 -0600

    Bug fix : getting fragment for result

--- src/com/jarp/tutorials/bigranchprohects/ch12/CrimeFragment.java
+++ src/com/jarp/tutorials/bigranchprohects/ch12/CrimeFragment.java
@@ -3,8 +3,11 @@
  */
 package com.jarp.tutorials.bigranchprohects.ch12;
 
+import java.util.Date;
 import java.util.UUID;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -28,8 +31,6 @@ import com.jarp.tutorials.bigranchprohects.R;
  */
 public class CrimeFragment extends Fragment {
 	
-
-
 	private Crime mCrime;
 	private CheckBox mSolvedCheckbox;
 	private Button mDateButton;
@@ -38,6 +39,23 @@ public class CrimeFragment extends Fragment {
 	
 	public static final String EXTRA_CRIME_ID ="EXTRA_CRIME_ID";
 	public static final String TAG_DIALOG_DATE = "FRAGMENT_PICK";
+	
+	
+	private static final int REQUEST_DATE = 0;
+
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if(resultCode!=Activity.RESULT_OK) return;
+		if(requestCode==REQUEST_DATE)
+		{
+			Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
+			mCrime.setDate(date);
+			updateDate();
+		}
+			
+	}
+
 	
 	
 	
@@ -103,10 +121,7 @@ public class CrimeFragment extends Fragment {
 		});
 		
 		mDateButton = (Button) v.findViewById(R.id.crime_date);
-		
-		
-		
-		mDateButton.setText(mCrime.getDate().toString());
+		updateDate();
 		
 		//mDateButton.setEnabled(false);
 		
@@ -118,6 +133,11 @@ public class CrimeFragment extends Fragment {
 			FragmentManager fm = getActivity().getSupportFragmentManager();
 			
 			DatePickerFragment dateFragment = DatePickerFragment.newInstance(mCrime.getDate());
+			
+			
+			dateFragment.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
+			
+			
 			
 			dateFragment.show(fm, TAG_DIALOG_DATE);
 			
@@ -143,9 +163,9 @@ public class CrimeFragment extends Fragment {
 	}
 
 
-	public void returnResult()
+	public void updateDate()
 	{
-		//getActivity().setResult(resultCode, data)
+		mDateButton.setText(mCrime.getDate().toString());
 	}
 	
 	
--- src/com/jarp/tutorials/bigranchprohects/ch12/DatePickerFragment.java
+++ src/com/jarp/tutorials/bigranchprohects/ch12/DatePickerFragment.java
@@ -7,10 +7,15 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.DatePicker.OnDateChangedListener;
@@ -26,6 +31,26 @@ public class DatePickerFragment extends DialogFragment {
 	public static final String EXTRA_DATE="com.jarp.tutorials.bigranchprojects.ch12.date";
 	
 	private Date mDate;
+	
+	private Fragment fg ;
+	
+	private int requestCode;
+	
+	
+	private void sendResult(int resultCode)
+	{
+		if(getTargetFragment()==null)
+			return ;
+		
+		Intent i   = new Intent();
+		
+		i.putExtra(EXTRA_DATE, mDate);
+		
+		
+		getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, i);
+		
+	}
+	
 	
 	public static DatePickerFragment newInstance(Date date)
 	{
@@ -78,7 +103,15 @@ public class DatePickerFragment extends DialogFragment {
 		
 		return new AlertDialog.Builder(getActivity())
 		.setTitle(R.string.date_picker_title)
-		.setPositiveButton(android.R.string.ok, null)
+		//.setPositiveButton(android.R.string.ok, null)
+		.setPositiveButton(android.R.string.ok, new OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				sendResult(Activity.RESULT_OK);
+			}
+		})
 		.setView(v)
 		.create();
 

0789700f77f573312d819a012c455b2844111951
JARP <ramirezp.jorge@gmail.com>
2014-01-29 22:14:25 -0600

    Adding no window feature finishing chapter 19

--- src/com/jarp/tutorials/bigranchprohects/ch19/CrimeCameraActivity.java
+++ src/com/jarp/tutorials/bigranchprohects/ch19/CrimeCameraActivity.java
@@ -3,7 +3,10 @@
  */
 package com.jarp.tutorials.bigranchprohects.ch19;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
 
 /**
  * @author JARP
@@ -11,12 +14,25 @@ import android.support.v4.app.Fragment;
  */
 public class CrimeCameraActivity extends SingleFragmentActivity {
 
+	@Override
+	public void onCreate(Bundle savedInstance)
+	{
+		
+		
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		super.onCreate(savedInstance);
+	}
+	
 	/* (non-Javadoc)
 	 * @see com.jarp.tutorials.bigranchprohects.ch19.SingleFragmentActivity#createFragment()
 	 */
 	@Override
 	protected Fragment createFragment() {
 		// TODO Auto-generated method stub
+		
 		return new CrimeCameraFragment();
 	}
 
